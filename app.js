7082aa085116d27fe69f43f8e54b86d7bbe5c0f0
Arun Sood <arsood@gmail.com>
2017-08-24 12:17:38 -0700

    Templating with template strings

--- app.js
+++ app.js
@@ -12,10 +12,36 @@ $(document).ready(function() {
         type: "GET",
         url: "http://myapi-profstream.herokuapp.com/api/99e553/persons"
     })
-    .then(function(data) {
+    .then(function(persons) {
         //Step 1: Loop through "persons" data
         //Step 2: Dynamically create HTML (table row) with data
         //Step 3: Append new HTML to DOM
+
+        var $tbody = $("tbody");
+
+        persons.forEach(function(person) {
+            var newHtml = `
+                <tr>
+                    <td>
+                        ${person.firstname}
+                    </td>
+                    <td>
+                        ${person.lastname}
+                    </td>
+                    <td>
+                        ${person.username}
+                    </td>
+                    <td>
+                        ${person.email}
+                    </td>
+                    <td>
+                        <a href="#" class="btn btn-primary">Edit</a>
+                    </td>
+                </tr>
+            `;
+
+            $tbody.append(newHtml);
+        });
     })
     .catch(function(err) {
         console.log(err);
